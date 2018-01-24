d91fb47f072a8868a2babafa867401308fa92076
Raymond Gan <rayning@yahoo.com>
2018-01-23 17:39:53 -0800

    Add react-router dependencies + 3 React components. May click from PhotoGrid on Main page to Single component.

---
+++ client/rayduxstagram.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+// Import css
+import css from './styles/style.styl';
+
+// Import components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+// Import react-router dependences
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+// if url = "/", show main PhotoGrid
+// if url = "/view/:postId", show Single (child of Main)
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
--- client/reduxstagram.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import { render } from 'react-dom';
-
-// Import css
-import css from './styles/style.styl';
-
-render(<p>hi</p>, document.getElementById('root'));
